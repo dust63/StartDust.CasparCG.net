279e7d634c064c5e218f44740a90480036583a37
Romain Jourde <dust63@hotmail.fr>
2019-02-17 00:10:02 +0100

    Change demo folder

--- src/StarDust.CasparCG.Demo.net/Properties/AssemblyInfo.cs
+++ src/StarDust.CasparCG.Demo.net/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("c414846e-4136-485b-886b-65e4af9a948b")]
+[assembly: Guid("e74efd1a-1d2f-49ea-b787-b76f4094edd1")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
