62e4be258af974d2210f8d314925fac0b45bce3f
dust63 <dust63@hotmail.fr>
2020-01-18 14:36:42 +0100

    Add documentation on channel layout

--- src/StarDust.CasparCG.net.Models/Channel/ChannelLayout.cs
+++ src/StarDust.CasparCG.net.Models/Channel/ChannelLayout.cs
@@ -4,41 +4,65 @@ using System.Runtime.Serialization;
 
 namespace StarDust.CasparCG.net.Models
 {
+    /// <summary>
+    /// Sound Layout configuration
+    /// <see cref="https://github.com/CasparCG/help/wiki/Media:-Audio"/>
+    /// </summary>
     [Serializable]
     public enum ChannelLayout
     {
+        /// <summary>
+        /// Mono [1.0] Layout: [C]
+        /// </summary>
         [Display(Name = "Mono")]
         [AMCPCommandValue("C")]
         [EnumMember]
         Mono,
 
-
+        /// <summary>
+        /// Stereo [2.0] Layout: [L R]
+        /// </summary>
         [Display(Name = "Stereo")]
         [AMCPCommandValue("L R")]
         [EnumMember]
         Stereo,
 
+        /// <summary>
+        /// DTS [5.1] Layout: [ C L R Ls Rs LFE]
+        /// </summary>
         [Display(Name = "DTS")]
         [AMCPCommandValue("C L R Ls Rs LFE")]
         [EnumMember]
         Dts,
 
-        [Display(Name = "DolbyE")]
+        /// <summary>
+        /// "Dolby E [5.1 + stereo mix] Layout: [L R C LFE Ls Rs Lmix Rmix]
+        /// </summary>
+        [Display(Name = "Dolby E")]
         [AMCPCommandValue("L R C LFE Ls Rs Lmix Rmix")]
         [EnumMember]
         DolbyE,
 
-        [Display(Name = "DolbyDigital")]
+        /// <summary>
+        /// Dolby Digital [5.1] Layout: [L C R Ls Rs LFE]
+        /// </summary>
+        [Display(Name = "Dolby Digital")]
         [AMCPCommandValue("L C R Ls Rs LFE")]
         [EnumMember]
         DolbyDigital,
 
+        /// <summary>
+        /// SMPTE [5.1] Layout: [L R C LFE Ls Rs]
+        /// </summary>
         [Display(Name = "SMPTE")]
         [AMCPCommandValue("L R C LFE Ls Rs")]
         [EnumMember]
         Smpte,
 
-        [Display(Name = "PassThrought")]
+        /// <summary>
+        /// Pass Throught [16ch] Layout: [will just pass everything as is]
+        /// </summary>
+        [Display(Name = "Pass Throught")]
         [AMCPCommandValue("passthru")]
         [EnumMember]
         Passthrought
