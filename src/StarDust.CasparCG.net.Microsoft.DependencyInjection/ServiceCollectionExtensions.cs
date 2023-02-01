205f50d08c81bcd372c3b0d3a32aef1bf854f04d
Romain Jourde <dust63@hotmail.fr>
2023-02-02 00:00:00 +0100

    Add project for Microsoft DI

---
+++ src/StarDust.CasparCG.net.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StarDust.CasparCG.net.Device;
+using StarDust.CasparCG.net.AmcpProtocol;
+using StarDust.CasparCG.net.Connection;
+
+namespace StarDust.CasparCG.net.Microsoft.DependencyInjections;
+
+/// <summary>
+/// Extension to configure dependencies of <see cref="CasparDevice"/>
+/// </summary>
+public static class ServiceCollectionExtensions
+{ 
+     public static IServiceCollection AddCasparCGDevice(this IServiceCollection services)
+     {
+        services.AddTransient<ICasparDevice, CasparDevice>();
+        services.AddTransient<IServerConnection, ServerConnection>();
+        services.AddTransient<IAMCPTcpParser, AmcpTCPParser>();
+        services.AddSingleton<IDataParser, CasparCGDataParser>();
+        services.AddSingleton<IAMCPProtocolParser, AMCPProtocolParser>();
+        
+        return services;
+    }
+}
