fc45b5101611ab28c9dde07ced0490583700b4ae
Romain Jourde <dust63@hotmail.fr>
2023-02-02 00:14:55 +0100

    update nuget packages

--- src/StarDust.CasparCG.net.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/StarDust.CasparCG.net.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using StarDust.CasparCG.net.Device;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using StarDust.CasparCG.net.AmcpProtocol;
 using StarDust.CasparCG.net.Connection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using StarDust.CasparCG.net.Device;
 
 namespace StarDust.CasparCG.net.Microsoft.DependencyInjections;
 
@@ -10,21 +10,21 @@ namespace StarDust.CasparCG.net.Microsoft.DependencyInjections;
 /// Extension to configure dependencies of <see cref="CasparDevice"/>
 /// </summary>
 public static class ServiceCollectionExtensions
-{ 
+{
     /// <summary>
     /// Add dependencies to interact with CasparCG.
     /// To interact with CasparCG play with the interface <see cref="ICasparDevice"/>
     /// </summary>
     /// <param name="services"></param>
     /// <returns></returns>
-     public static IServiceCollection AddCasparCG(this IServiceCollection services)
-     {
+    public static IServiceCollection AddCasparCG(this IServiceCollection services)
+    {
         services.TryAddTransient<ICasparDevice, CasparDevice>();
         services.TryAddTransient<IServerConnection, ServerConnection>();
         services.TryAddTransient<IAMCPTcpParser, AmcpTCPParser>();
         services.TryAddSingleton<IDataParser, CasparCGDataParser>();
         services.TryAddSingleton<IAMCPProtocolParser, AMCPProtocolParser>();
-        
+
         return services;
     }
 }
