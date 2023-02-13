0e8c6212f176d7a01a2049caf4586d35d326e973
Romain Jourde <dust63@hotmail.fr>
2023-02-14 00:24:59 +0100

    Remove useless exception

--- src/StarDust.CasparCG.net.RestApi/Exceptions/AmqpServerDidNotResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace StarDust.CasparCG.net.RestApi.Exceptions;
-
-public class AmqpServerDidNotResponse : InvalidOperationException
-{   
-    public AmqpServerDidNotResponse(string hostname):base($"The CasparCG server is not running on host: {hostname}.")
-    {        
-    }
-}
