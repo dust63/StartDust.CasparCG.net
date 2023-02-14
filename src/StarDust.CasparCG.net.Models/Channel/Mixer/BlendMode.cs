624595e045cb52f2353ffc92e1c78e6945281b22
Romain Jourde <romain.jourde@fromeathtolife.net>
2023-02-14 14:14:20 +0100

    Add missing doc on blend mode

--- src/StarDust.CasparCG.net.Models/Channel/Mixer/BlendMode.cs
+++ src/StarDust.CasparCG.net.Models/Channel/Mixer/BlendMode.cs
@@ -5,34 +5,149 @@
     /// </summary>
     public enum BlendMode
     {
+        /// <summary>
+        /// Normal
+        /// </summary>
         Normal,
+
+        /// <summary>
+        /// Lighten
+        /// </summary>
         Lighten,
+
+        /// <summary>
+        /// Darken
+        /// </summary>
         Darken,
+
+        /// <summary>
+        /// Multiply
+        /// </summary>
         Multiply,
+
+        /// <summary>
+        /// Average
+        /// </summary>
         Average,
+
+        /// <summary>
+        /// Add
+        /// </summary>
         Add,
+
+        /// <summary>
+        /// Substract
+        /// </summary>
         Substract,
+
+        /// <summary>
+        /// Difference
+        /// </summary>
         Differance,
+
+        /// <summary>
+        /// Negation
+        /// </summary>
         Negation,
+
+        /// <summary>
+        /// Exclusion
+        /// </summary>
         Exclusion,
+
+        /// <summary>
+        /// Screnn
+        /// </summary>
         Screen,
+
+        /// <summary>
+        /// Overlay
+        /// </summary>
         Overlay,
+        
+        /// <summary>
+        /// Soft light
+        /// </summary>
         Soft_Light,
+
+        /// <summary>
+        /// Hard light
+        /// </summary>
         Hard_Light,
+
+        /// <summary>
+        /// Color dodge
+        /// </summary>
         Color_Dodge,
+
+        /// <summary>
+        /// Color burn
+        /// </summary>
         Color_Burn,
+
+        /// <summary>
+        /// Linear dodge
+        /// </summary>
         Linear_Dodge,
+
+        /// <summary>
+        /// Linear burn
+        /// </summary>
         Linear_Burn,
+
+        /// <summary>
+        /// Linear light
+        /// </summary>
         Linear_Light,
+
+        /// <summary>
+        /// Vivid light
+        /// </summary>
         Vivid_Light,
+
+        /// <summary>
+        /// Pin light
+        /// </summary>
         Pin_Light,
+
+        /// <summary>
+        /// Hard mix
+        /// </summary>
         Hard_Mix,
+
+        /// <summary>
+        /// Reflect
+        /// </summary>
         Reflect,
+
+        /// <summary>
+        /// Glow
+        /// </summary>
         Glow,
+
+        /// <summary>
+        /// Phoenix
+        /// </summary>
         Phoenix,
+
+        /// <summary>
+        /// Contrast
+        /// </summary>
         Contrast,
+
+        /// <summary>
+        /// Saturation
+        /// </summary>
         Saturation,
+
+        /// <summary>
+        /// Color
+        /// </summary>
         Color,
+
+        /// <summary>
+        /// Lumninosity
+        /// </summary>
         Luminosity,
     }
 }
