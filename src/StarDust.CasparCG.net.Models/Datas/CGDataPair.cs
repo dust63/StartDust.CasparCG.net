4dd4df33240ca874ffe52871ae43db16db5125fc
Romain Jourde <romain.jourde@fromeathtolife.net>
2023-02-14 14:36:18 +0100

    Add doc on cg data pair

--- src/StarDust.CasparCG.net.Models/Datas/CGDataPair.cs
+++ src/StarDust.CasparCG.net.Models/Datas/CGDataPair.cs
@@ -9,26 +9,44 @@ namespace StarDust.CasparCG.net.Models
     [Serializable]
     public class CGDataPair
     {
-
+        /// <summary>
+        /// Create empty <see cref="CGDataPair"/>
+        /// </summary>
         public CGDataPair()
         {
         }
 
+        /// <summary>
+        /// Create <see cref="CGDataPair"/> whith name and data
+        /// <paramref name="name"/>
+        /// <paramref name="data"/>
+        /// </summary>
         public CGDataPair(string name, ICGComponentData data)
         {
             this.Name = name;
             this.Data = data;
         }
 
+        /// <summary>
+        /// Create <see cref="CGDataPair"/> whith name and string value
+        /// </summary>
+        /// <param name="name">name of data</param>
+        /// <param name="value">string value</param>
         public CGDataPair(string name, string value)
         {
             this.Name = name;
             this.Value = value;
         }
 
+        /// <summary>
+        /// Name of the data store
+        /// </summary>
         [DataMember]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Value stored
+        /// </summary>
         [DataMember]
         public string Value
         {
@@ -45,11 +63,17 @@ namespace StarDust.CasparCG.net.Models
             }
         }
 
+        /// <summary>
+        /// Data stored
+        /// </summary>
         public ICGComponentData Data { get; set; }
 
      
-
-        private string ToXml()
+        /// <summary>
+        /// Serialize to xml
+        /// </summary>
+        /// <returns></returns>
+        public string ToXml()
         {
             if (this.Data == null)
                 return null;
