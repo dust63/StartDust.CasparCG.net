a53fc12a4ce29af557928e72d3146abec769ad12
dust63 <romain.jourde63@gmail.com>
2026-06-04 22:25:26 +0200

    test: add query parser scaffold

---
+++ src/StarDust.CasparCG/Query/CasparQueryResultParser.cs
@@ -0,0 +1,8 @@
+namespace StarDust.CasparCG.Query;
+
+/// <summary>
+/// Projects AMCP query responses into higher-level query result objects.
+/// </summary>
+public static class CasparQueryResultParser
+{
+}
